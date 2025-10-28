e0f3f18fcf47e361862ba2a9ccac53b2cee95ed9
Benjamin Zamudio <bzamudio@teamworkcommerce.com>
2025-10-28 16:46:46 -0600

    practica de como crear un GKE usando Terraform.

---
+++ GettingStarted/GKE/variables.tf
@@ -0,0 +1,22 @@
+variable "project_id" {
+  description = "El ID del proyecto de Google Cloud."
+  type        = string
+}
+
+variable "region" {
+  description = "La región de Google Cloud donde se creará el clúster."
+  type        = string
+  default     = "us-central1"
+}
+
+variable "cluster_name" {
+  description = "El nombre deseado para el clúster GKE."
+  type        = string
+  default     = "mi-cluster-gke"
+}
+
+variable "machine_type" {
+  description = "El tipo de máquina para los nodos del clúster (por ejemplo, e2-medium)."
+  type        = string
+  default     = "e2-medium"
+}
