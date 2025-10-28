7bf4421df6f151dee4360785e5cc3a7a2bfd2ebc
Benjamin Zamudio <bzamudio@teamworkcommerce.com>
2025-10-28 12:59:05 -0600

    getting started practice with terraform & GCP.

--- GettingStarted/terraform/1-provider.tf
+++ GettingStarted/terraform/1-provider.tf
@@ -4,12 +4,12 @@ provider "google" {
 }
 
 terraform {
-  required_version = ">= 4.0"
+  required_version = ">= 1.0"
 
   required_providers {
     google = {
         source = "hashicorp/google"
-        version = "~> 5.0"
+        version = "~> 6.0"
     }
   }
 }
